93d5b16959521321fa51577279b73d13f704515b
skr <skrint301@gmail.com>
2024-11-17 01:07:03 +0530

    Added transparent overlay

--- app/src/main/java/com/skrinternationals/chesstimer/MainActivity.java
+++ app/src/main/java/com/skrinternationals/chesstimer/MainActivity.java
@@ -1,13 +1,15 @@
 package com.skrinternationals.chesstimer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.util.Locale;
 
@@ -19,9 +21,11 @@ public class MainActivity extends AppCompatActivity {
     TextView textview_player2_second;
     ImageView imageView_overlay;
 
+    boolean is_running = false;
     boolean player1_timer_is_on = false;
 
-    CountDownTimer timer;
+    CountDownTimer timer1;
+    CountDownTimer timer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,51 +35,71 @@ public class MainActivity extends AppCompatActivity {
         initialize();
 
         imageView_overlay.setOnClickListener(v-> {
-            if ((player1_timer_is_on)) {
-                startTimerForPlayer2();
-            } else {
+            if (!is_running) {
                 startTimerForPlayer1();
+                is_running = true;
+                imageView_overlay.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            } else {
+                if (player1_timer_is_on) {
+                    stopTimerForPlayer1();
+                    startTimerForPlayer2();
+                } else {
+                    stopTimerForPlayer2();
+                    startTimerForPlayer1();
+                }
             }
             player1_timer_is_on = !player1_timer_is_on;
         });
+    }
 
+    private void stopTimerForPlayer1() {
+        timer1.cancel();
+    }
+
+    private void stopTimerForPlayer2() {
+        timer2.cancel();
     }
 
     private void startTimerForPlayer1() {
-        long player1_time_in_millis = Integer.parseInt((String) textview_player1_minute.getText()) * 60L + Integer.parseInt((String) textview_player1_minute.getText());
-        timer = new CountDownTimer(player1_time_in_millis, 1000) {
+        long player1_time_in_millis = (Integer.parseInt((String) textview_player1_minute.getText()) * 60L + Integer.parseInt((String) textview_player1_second.getText())) * 1000;
+        timer1 = new CountDownTimer(player1_time_in_millis, 1000) {
 
             @Override
             public void onTick(long millis_until_finished) {
                 long minutes = (millis_until_finished / 1000) / 60;
                 long seconds = (millis_until_finished / 1000) % 60;
 
-                textview_player1_minute.setText((int) minutes);
-                textview_player1_second.setText((int) seconds);
+                textview_player1_minute.setText(String.format(Locale.ENGLISH, "%02d", minutes));
+                textview_player1_second.setText(String.format(Locale.ENGLISH, "%02d", seconds));
             }
 
             @Override
             public void onFinish() {
+//                reset
+                is_running = false;
+                imageView_overlay.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.transparent));
                 Toast.makeText(MainActivity.this, "Time's up", Toast.LENGTH_SHORT).show();
             }
         }.start();
     }
 
     private void startTimerForPlayer2() {
-        long player2_time_in_millis = Integer.parseInt((String) textview_player2_minute.getText()) * 60L + Integer.parseInt((String) textview_player2_minute.getText());
-        timer = new CountDownTimer(player2_time_in_millis, 1000) {
-
+        long player2_time_in_millis = (Integer.parseInt((String) textview_player2_minute.getText()) * 60L + Integer.parseInt((String) textview_player2_second.getText())) * 1000;
+        timer2 = new CountDownTimer(player2_time_in_millis, 1000) {
             @Override
             public void onTick(long millis_until_finished) {
                 long minutes = (millis_until_finished / 1000) / 60;
                 long seconds = (millis_until_finished / 1000) % 60;
 
-                textview_player2_minute.setText((int) minutes);
-                textview_player2_second.setText((int) seconds);
+                textview_player2_minute.setText(String.format(Locale.ENGLISH, "%02d", minutes));
+                textview_player2_second.setText(String.format(Locale.ENGLISH, "%02d", seconds));
             }
 
             @Override
             public void onFinish() {
+//                reset
+                is_running = false;
+                imageView_overlay.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.transparent));
                 Toast.makeText(MainActivity.this, "Time's up", Toast.LENGTH_SHORT).show();
             }
         }.start();
